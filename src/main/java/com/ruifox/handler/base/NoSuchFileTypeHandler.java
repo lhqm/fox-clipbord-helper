3037c3bec0085b9b06087befb2ce08af77d58aba
lhqm <1229796683@qq.com>
2024-02-05 15:15:47 +0800

    增加了不支持的文件类型的导入报错

--- src/main/java/com/ruifox/handler/base/NoSuchFileTypeHandler.java
+++ src/main/java/com/ruifox/handler/base/NoSuchFileTypeHandler.java
@@ -8,15 +8,16 @@ import java.io.File;
 public class NoSuchFileTypeHandler extends FileHandler{
     @Override
     protected boolean canHandle(FileRequest request) {
-//        都能处理，走到这一步了必须处理掉
+//        都能处理，因为走到这一步了必须全部处理掉
         return true;
     }
 
     @Override
     protected String processRequest(FileRequest request) {
+        System.out.println("进入后置文件处理器,文件将被放弃解析.");
 //        删文件
         boolean delete = new File(request.getFilePath()).delete();
 //        返回错误报告
-        return String.format("不支持解析的文档类型,文件 %s 成功删除",delete?"已":"未");
+        return String.format("不支持解析的文档类型,文件%s成功删除",delete?"已":"未");
     }
 }

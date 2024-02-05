0d5b807ce8a6b4f022d60fff5d69e845c940641d
lhqm <1229796683@qq.com>
2024-02-05 15:39:39 +0800

    优化了责任链的注入逻辑

--- src/main/java/com/ruifox/handler/base/FileHandlerFactory.java
+++ src/main/java/com/ruifox/handler/base/FileHandlerFactory.java
@@ -4,6 +4,9 @@ import com.ruifox.handler.DOCHandler;
 import com.ruifox.handler.DOCXHandler;
 import com.ruifox.handler.PDFHandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class FileHandlerFactory {
@@ -26,18 +29,33 @@ public class FileHandlerFactory {
     }
 
     private static FileHandler fileProcessFilterRegistry(){
-        // 创建DOCHandler和DOCXHandler的实例
-        FileHandler docHandler = new DOCHandler();
-        FileHandler docxHandler = new DOCXHandler();
-        // 设置DOCXHandler的下一个处理器为DOCHandler
-        docxHandler.setNextHandler(docHandler);
-//        设置doc后一个处理器为PDF处理器
-        PDFHandler pdfHandler = new PDFHandler();
-        docHandler.setNextHandler(pdfHandler);
+//        // 创建DOCHandler和DOCXHandler的实例
+//        FileHandler docHandler = new DOCHandler();
+//        FileHandler docxHandler = new DOCXHandler();
+//        // 设置DOCXHandler的下一个处理器为DOCHandler
+//        docxHandler.setNextHandler(docHandler);
+////        设置doc后一个处理器为PDF处理器
+//        PDFHandler pdfHandler = new PDFHandler();
+//        docHandler.setNextHandler(pdfHandler);
+//
+//        // 最后，兜底的处理器会处理一切
+//        NoSuchFileTypeHandler noSuchFileTypeHandler = new NoSuchFileTypeHandler();
+//        pdfHandler.setNextHandler(noSuchFileTypeHandler);
+//        return docxHandler;
+//        构造处理器链
+        return constructorChain(
+                List.of(new DOCHandler(),new DOCXHandler(),new PDFHandler())
+        );
+    }
 
-        // 最后，兜底的处理器会处理一切
-        NoSuchFileTypeHandler noSuchFileTypeHandler = new NoSuchFileTypeHandler();
-        pdfHandler.setNextHandler(noSuchFileTypeHandler);
-        return docxHandler;
+    private static <E extends FileHandler> FileHandler constructorChain(List<E> handlers){
+//        循环添加处理器
+        for (int i = 0; i < handlers.size()-1; i++) {
+            handlers.get(i).setNextHandler(handlers.get(i+1));
+        }
+//        最后一个处理器，加上非空处理器
+        handlers.get(handlers.size()-1).setNextHandler(new NoSuchFileTypeHandler());
+//        返回第一个处理器
+        return handlers.get(0);
     }
 }
